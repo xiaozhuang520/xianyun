5342a891842b46ec44b2f5684333f214a5a6312b
xiaozhuang <zhuangjinton@163.com>
2019-10-11 15:20:43 +0800

    初始化

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default {
+    data(){
+        return {
+            // 轮播图数据
+            banners: [
+                {
+                    url: "http://157.122.54.189:9095/assets/images/th03.jfif",
+                },
+                {
+                    url: "http://157.122.54.189:9095/assets/images/th04.jfif",
+                }
+            ]
+        }
+    }
+}
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  首页
+    </div>
+</template>
+
+<script>
+
+import config from './index.js'
+export default config
+</script>
+
+<style scoped lang="less">
+@import url('./index.less');
+</style>
